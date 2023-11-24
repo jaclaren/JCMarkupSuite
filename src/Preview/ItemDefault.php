5eee65fd33146d5661af92b3e5ea11a6b6933813
Jani <jani.claren@gmail.com>
2023-11-24 10:16:05 +0100

    Refactor ItemDefault for customization.

--- src/Preview/ItemDefault.php
+++ src/Preview/ItemDefault.php
@@ -16,28 +16,63 @@ class ItemDefault
     public static function render($title = '', $excerpt = '', $coverimage = '', $link = '')
     {
         ob_start();
-        ?>
+?>
         <div class="preview-list__item xs:flex">
             <a href="<?= $link; ?>">
-                <div class="preview-list__item__image">
-                    <?php # if thumbnail not availabl,e use coverimage ?>
-                    <?Php if (has_post_thumbnail($post->ID)) : ?>
-                        <?= get_the_post_thumbnail($post->ID, 'medium'); ?>
-                    <?php else : ?>
-                        <img class="preview-list__item__image__coverimage" src="<?= $coverimage; ?>" alt="<?= $post->post_title; ?>">
-                        <div class="preview-list__item__image__placeholder" style="background: url(<?= $coverimage; ?>); ">
-                        </div>
-                    <?php endif; ?>                    
-                </div>
-                <div class="preview-list__item__content">
-                    <h3><?= $title; ?></h3>
-                    <p><?= $excerpt; ?></p>
-                </div>
+                <?= self::renderImage($title, $excerpt, $coverimage, $link); ?>
+                <?= self::renderContent($title, $excerpt, $coverimage, $link); ?>
             </a>
         </div>
-        <?php
+    <?php
         return ob_get_clean();
     }
-}
 
-    
+    /**
+     * Render the preview list image
+     * 
+     * @param string $title 
+     * @param string $excerpt 
+     * @param string $coverimage
+     * @param string $link
+     * 
+     */
+
+    public static function renderImage($title = '', $excerpt = '', $coverimage = '', $link = '')
+    {
+        ob_start();
+    ?>
+        <div class="preview-list__item__image">
+            <img src="<?= $coverimage; ?>" alt="<?= $title; ?>">
+        </div>
+
+<?php
+        return ob_get_clean();
+    }
+
+    /**
+     * Render the preview list content
+     * 
+     * @param string $title 
+     * @param string $excerpt 
+     * @param string $coverimage
+     * @param string $link
+     * 
+     */
+
+    public static function renderContent($title = '', $excerpt = '', $coverimage = '', $link = '')
+    {
+        ob_start();
+
+?>
+        <div class="preview-list__item__content">
+            <h3 class="preview-list__item__content__title">
+                <?= $title; ?>
+            </h3>
+            <p class="preview-list__item__content__excerpt">
+                <?= $excerpt; ?>
+            </p>
+        </div>
+<?php
+        return ob_get_clean();
+    }    
+}
