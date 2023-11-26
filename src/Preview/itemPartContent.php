1ba56476d693a8e85ee2fcd3bf74c4c33b9dd8af
Jani <jani.claren@gmail.com>
2023-11-26 13:13:09 +0100

    Refactor: Introduced separate generateHTML function for isolation of generation logic.

--- src/Preview/itemPartContent.php
+++ src/Preview/itemPartContent.php
@@ -22,34 +22,72 @@ class ItemPartContent
         - Title and Meta colunns        
     */
 
+    /**
+     * Constructor
+     * 
+     * @param string $text
+     */
+
     public function __construct($text = '')
     {
         $this->text = $text;
     }
 
-    /** Different variations: with score, without score */
+    /**
+     * Function to add a score
+     * 
+     * @param IScore $score
+     * 
+     */
 
     public function addScore(IScore $score)
     {
         $this->score = $score;
     }
 
-    /** Function to add a meta row */
+    /**
+     * Function to add a meta row
+     * 
+     * @param ItemPartMetaRow $metaRow
+     */
 
     public function addMetaRow(ItemPartMetaRow $metaRow)
     {
         $this->metaRows[] = $metaRow;
     }
 
+    public function generateHTML()
+    {
+?>
+        <div class="preview-list__item__content">
+            <?php if ($this->score) : ?>
+                <div class="preview-list__item__content__col preview-list__item__content__score">
+                    <?= $this->score->render(); ?>
+                </div>
+            <?php endif; ?>
+
+            <?php if (count($this->metaRows) > 0) : ?>
+                <div class="flex flex-v">
+                    <h3><?= $this->text; ?></h3>
+                    <div class="preview-list__item__content__col preview-list__item__content__metas">
+                        <?php foreach ($this->metaRows as $metaRow) : ?>
+                            <?= $metaRow->render(); ?>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            <?php else : ?>
+                <div class="preview-list__item__content__col preview-list__item__content__text">
+                    <h3><?= $this->text; ?></h3>
+                </div>
+            <?php endif; ?>
+        </div>
+<?php
+    }
+
     public function render()
     {
-
-        ob_start(); ?>
-        <div class="preview-list__item__content">
-            <h3><?= $this->text; ?></h3>
-        </div>
-
-<?php
+        ob_start();
+        $this->generateHTML();
         return ob_get_clean();
     }
 }
